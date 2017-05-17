ef7a30d8cc400184b4f4143ba6cae9258185bac7
Jason Rahm <jason.rahm@ucdenver.edu>
2017-05-17 16:15:02 -0600

    initial custom style sheet created

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var concat = require('gulp-concat'),
+    concatCss = require('gulp-concat-css'),
+    cssnano = require('gulp-cssnano'),
+    del = require('del'),
+    gulp = require('gulp'),
+    rename = require('gulp-rename'),
+    runSequence = require('run-sequence'),
+    uglify = require('gulp-uglify');
+
+
+
+/*********************************************
+            Global Tasks
+*********************************************/
+
+gulp.task('clean:dist', function() {
+  return del.sync('dist');
+});
+
+gulp.task('minify-css', function() {
+  return gulp.src(['dev/styles.css'])
+    .pipe(concatCss('univ-intranet-seattle.css'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist'));
+});
