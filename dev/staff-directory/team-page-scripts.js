e3ebc0a12530f4818ae25d60ee8282753e2adafa
Jason Rahm <jason.rahm@ucdenver.edu>
2017-08-17 10:53:37 -0600

    initial comments/notes made for dynamic team page creation

---
+++ dev/staff-directory/team-page-scripts.js
@@ -0,0 +1,8 @@
+//https://mycon.ucdenver.edu/_api/web/lists/GetByTitle('Staff Directory')/items?$filter=Team eq 'Marketing'
+//Call directory list and filter for each team so you only have the individaul team to work with on the page instead of the whole list
+//May have to refactor directory ajax call to accept string for team name
+//How do you assign the team name on the page or in the template so it is reusable and dynamic for the team?
+//Data on an html element?
+//hidden dropdown?
+//H1 text == team name?
+//URL indexof?
