132a878c7fe366453ce2771294cf5fd31cdb561c
Jason Rahm <jason.rahm@ucdenver.edu>
2017-08-24 09:10:45 -0600

    html component creation functions added

--- dev/service-list/service-list-scripts.js
+++ dev/service-list/service-list-scripts.js
@@ -40,7 +40,8 @@ var getEmails = function() {
       
     })
    .error(function() {
-      reject('Error with getEmails()');
+      console.log('Error with getEmails()');
+      reject();
     });
   });
 };
@@ -68,10 +69,10 @@ var getServices = function() {
       //Upon success, the list data alpha sorted
         //List call results limited to 100 by default append ?$top=(up to 1000) to url to extend results
           //https://sharepoint.stackexchange.com/questions/74777/list-api-get-all-items-limited-to-100-rows
+            
       results.sort(function(a, b) {
         return (a.Title > b.Title) ? 1 : ((b.Title > a.Title) ? -1 : 0);
       });
-        
       
       //Each item is turned into a JSON Object and pushed into the allStaff array
       var teams = {};
@@ -88,16 +89,72 @@ var getServices = function() {
         services.push({team: teamName, list: teams[teamName], email: emails[teamName]});
       }
       
+      //Object array is alpha sorted by team name
+      services.sort(function(a, b) {
+        return (a.team > b.team) ? 1 : ((b.team > a.team) ? -1 : 0);
+      });
+      
       //After data is sorted and object created, the promise resolves so the next action can occur
       resolve();
       
     })
    .error(function() {
-      reject('Error with getServices()');
+      console.log('Error with getServices()');
+      reject();
     });
   });
   
 };
+
+
+
+/**************************************************************************
+                      Component Creation Functions
+**************************************************************************/
+
+function createList(x) {
+  
+  var list = '<ul class="list-column">';
+  
+  function linkCheck(y) {
+    return y.link ? '<a href="' + y.link + '">' + y.service + '</a>' : y.service;
+  }
+  
+  for(var i = 0; i < x.length; i++) {
+    list += '<li>' + linkCheck(x[i]) + '</li>';
+  }
+  
+  list += '</ul>';
+  
+  return list;
+
+}
+
+function createEmail(x) {
+  if(x !== undefined) {
+    var emailSection = '<section class="margin-top-small"><h3>Department Contact</h3><ul class="no-bullets">';
+    
+    for(var i = 0; i < x.length; i++) {
+      emailSection += '<li><a href="mailto:' + x[i] + '">' + x[i] + '</a></li>';
+    }
+    
+    emailSection += '</ul></section>';
+    
+    return emailSection;
+    
+  } else {
+    return '';
+  }
+}
+
+function buildServiceSections(x) {
+  var serviceSection = '<section class="width-45 margin-vertical-medium"><h2>' + x.team + '</h2>';
+      serviceSection += createList(x.list);
+      serviceSection += createEmail(x.email);
+      serviceSection += '</section>';
+  $('#serviceListContainer').append(serviceSection);
+}
+
 
 
 /**************************************************************************
@@ -108,10 +165,18 @@ $(document).ready(function() {
   getEmails()
     .then(getServices)
     .then(function() {
-      console.log(emails);
       console.log(services);
+
+      for(var i = 0; i < services.length; i++) {
+        buildServiceSections(services[i]);
+      }
+    
+      $('#loadingMessage').remove();
+      $('#serviceListContainer').addClass('flex-container').removeClass('hidden');
+    
     })
     .catch(function(reason) {
+      $('#loadingMessage').html('<h2>Data failed to load</h2>')
       console.log(reason)
   });
   
