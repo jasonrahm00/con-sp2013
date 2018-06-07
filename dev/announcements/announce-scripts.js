9783c42285533d6945e3844888620655eed72f72
Jason Rahm <jason.rahm@ucdenver.edu>
2018-06-07 10:37:52 -0600

    jquery logic and markup replaced with angularjs

--- dev/announcements/announce-scripts.js
+++ dev/announcements/announce-scripts.js
@@ -1,87 +1,20 @@
-/**************************************************************************
-                     Global Functions and Variables
-**************************************************************************/
+angular.module("announcements", [])
+.service('dataService', function($http, $q){
 
-var data = [],
-    listUrl = "https://mycon.ucdenver.edu/_vti_bin/listdata.svc/InternalAnnouncements",
-    pageUrl = window.location.href,
-    catFilters = {
-      "all": [
-        "General Announcements",
-        "Faculty Support",
-        "Grand Rounds",
-        "Promotion & Tenure",
-        "Compensation & Payroll",
-        "Performance Evaluations",
-        "Personnel Changes",
-        "Recruitment",
-        "Training & Development"
-      ],
-      "hr": [
-        "General Announcements",
-        "Compensation & Payroll",
-        "Performance Evaluations",
-        "Personnel Changes",
-        "Recruitment",
-        "Training & Development"
-      ],
-      "facAffairs": [
-        "General Announcements",
-        "Faculty Support",
-        "Grand Rounds",
-        "Promotion & Tenure",
-        "Compensation & Payroll",
-        "Performance Evaluations",
-        "Personnel Changes",
-        "Recruitment",
-        "Training & Development"
-      ]
-    };
-
-//Clears out directory container whenever called
-
-$(document).ready(function() {
-
-  function cleanContainer() {
-    $('#cardContainer').html('');
-  }
+  'use strict';
 
   function getNum(str) {
     return str.replace(/\D/g,"");
   }
 
-  function createAnnounceCard(arr) {
-    arr.forEach(function(obj) {
-      var announceCard = "<div class='card'>"; // begin card
-          announceCard += "<div class='card-header'>"; // begin card header
-          announceCard += "<img src='https://mycon.ucdenver.edu/PublishingImages/icons/megaphone-blue.png' alt=''>";
-          announceCard += "<span class='card-title'>" + obj.title + "</span>";
-          announceCard += "<span class='card-cat'>" + obj.category + "</span>";
-          announceCard += "</div>"; // end card header
-          announceCard += "<div class='card-content'>"; // begin card content
-          announceCard += obj.content;
-          announceCard += "<span class='card-date'>" + obj.created + "</span>";
-          announceCard += "</div>"; // end card content
-        announceCard += "</div>"; // end card
+  this.getData = function() {
 
-      $("#cardContainer").append(announceCard);
-    });
-  }
+    var deferred = $q.defer();
 
-  var getData = function() {
-
-    //Promise created to handle the ajax call
-    return new Promise(function(resolve, reject) {
-      $.ajax({
-        url: listUrl,
-        type: "GET",
-        headers: {
-          "accept": "application/json;odata=verbose"
-        }
-      })
-      .success(function (obj) {
-        //Upon success, the data (an array of strings) is pushed into the data array
-        $.each(obj.d.results, function(index, value) {
+    return $http.get('https://mycon.ucdenver.edu/_vti_bin/listdata.svc/InternalAnnouncements')
+      .then(function(response) {
+        var data = [];
+        response.data.d.results.forEach(function(value, index) {
           if(value.CategoryValue !== "Dean Message") {
             data.push(
               {
@@ -93,24 +26,64 @@ $(document).ready(function() {
               }
             );
           }
-
         });
-        resolve();
-      })
-      .error(function () {
-        reject(err);
-      });
+        deferred.resolve(data);
+        return deferred.promise;
+    }, function(error) {
+        deferred.reject(error);
+        return deferred.promise;
     });
-
   };
 
-  getData()
-    .then(function() {
-      cleanContainer();
-      createAnnounceCard(data);
-    })
-    .catch(function(err) {
-      console.log(err);
+})
+.filter("renderHTMLCorrectly", function($sce) {
+	return function(stringToParse) {
+      return $sce.trustAsHtml(stringToParse);
+	}
+})
+.controller("mainController", function($scope, dataService){
+
+  $scope.catFilters = {
+    "all": [
+      "General Announcements",
+      "Faculty Support",
+      "Grand Rounds",
+      "Promotion & Tenure",
+      "Compensation & Payroll",
+      "Performance Evaluations",
+      "Personnel Changes",
+      "Recruitment",
+      "Training & Development"
+    ],
+    "hr": [
+      "General Announcements",
+      "Compensation & Payroll",
+      "Performance Evaluations",
+      "Personnel Changes",
+      "Recruitment",
+      "Training & Development"
+    ],
+    "facAffairs": [
+      "General Announcements",
+      "Faculty Support",
+      "Grand Rounds",
+      "Promotion & Tenure",
+      "Compensation & Payroll",
+      "Performance Evaluations",
+      "Personnel Changes",
+      "Recruitment",
+      "Training & Development"
+    ]
+  };
+
+  $scope.chosenFilter = "All";
+
+  $scope.allAnnounce = [];
+
+  dataService.getData().then(function(response) {
+    $scope.allAnnounce = response;
+  }, function(error) {
+    console.log(error);
   });
 
 });
